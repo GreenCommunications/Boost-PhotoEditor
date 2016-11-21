e2b2f08429143d8adc6a4306c02a42b8f82e6109
GreenCommunications <alexander.m.rankin@gmail.com>
2016-11-21 14:26:45 -0500

    Create Plugin.cs

---
+++ boost/Plugin.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;

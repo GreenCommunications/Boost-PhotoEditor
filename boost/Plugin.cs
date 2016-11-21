bd52828a2781a2e3c114097485f41de0362d2775
GreenCommunications <alexander.m.rankin@gmail.com>
2016-11-21 14:33:28 -0500

    Started plugin platform

--- boost/Plugin.cs
+++ boost/Plugin.cs
@@ -6,3 +6,33 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+
+namespace boost
+{
+  class PluginPlatform
+  {
+    Assembly dll;
+    
+    Type type;
+    
+    object inst;
+    
+    public void PluginPlatform(string DLLLocation, string type_)
+    {
+      dll = Assembly.LoadFrom(DLLLocation);
+      type = dll.GetType(type_);
+      
+      inst = Activator.CreateInstance(type);
+    }
+    
+    public void CallSetupMethod()
+    {
+      inst.Setup();
+    }
+    
+    public void CallMainLoop(object[] parameters)
+    {
+      inst.Main(paramaters);
+    }
+  }
+}
